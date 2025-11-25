d6ae655eb5c3dd760f0e171d8e7538f27c48a0e4
Lexivo <culater753@gmail.com>
2025-11-24 18:24:28 -0600

    Adjusted methods to have less than 40 lines each

--- ProjectTask3_2025/MyLexer.rb
+++ ProjectTask3_2025/MyLexer.rb
@@ -3,121 +3,142 @@
 require_relative "MyToken"
 class MyLexer
 
+  # Initializes the class
   def initialize
     @input = $stdin.gets.chomp
     @position = 0
     @current_token = @input[@position]
+    @token_text = ''
   end
 
+  # Calls methods to determine token type and text
   def next_token
     token_type = -1
-    token_text = ''
     # Checks for EOF condition
     if @current_token.nil? == false
       case @current_token
-
         # Checks for strings and while keyword
       when /[A-Za-z]/
-          token_text = @current_token
-          continue
-          while @current_token =~ /[A-Za-z]/ do
-            token_text = token_text + @current_token
-            continue
-          end
-          if token_text == 'while'
-            token_type = 0
-          else
-            token_type = 1
-          end
-
+        token_type = string_kwrd
           # Checks for integers and floats
       when /[-?0-9]/
-        token_text = @current_token
-        continue
-        while @current_token =~ /[0-9]/ do
-          token_text = token_text + @current_token
-          continue
-        end
-        if @current_token == '.'
-          token_text = token_text + @current_token
-          continue
-          while @current_token =~ /[0-9]/ do
-            token_text = token_text + @current_token
-            continue
-          end
-          token_type = 3
-        else
-          token_type = 2
-        end
-
+        token_type = numbers
         # Checks comparisons
-      when '='
-        continue
-        if @current_token == '='
-          token_type = 4
-          token_text = "=="
-          continue
-        else
-          token_type = -99
-          token_text = '='
-        end
-
-
-      when '<'
-        continue
-        if @current_token == '='
-          token_type = 6
-          token_text = '<='
-          continue
-        else
-          token_type = 5
-          token_text = '<'
-        end
-
-
-      when '>'
-        continue
-        if @current_token == '='
-          token_type = 8
-          token_text = '>='
-          continue
-        else
-          token_type = 7
-          token_text = '>'
-        end
+      when '=','<','>'
+        token_type = comparisons(@current_token)
 
         # Checks other valid tokens
-      when '('
-        token_type = 14
-        token_text = @current_token
-        continue
-      when ')'
-        token_type = 15
-        token_text = @current_token
-        continue
-      when '{'
-        token_type = 16
-        token_text = @current_token
-        continue
-      when '}'
-        token_type = 17
-        token_text = @current_token
-        continue
-      when ';'
-        token_type = 18
-        token_text = @current_token
-        continue
-
+      when '(' , ')' , '{' , '}' , ';'
+        token_type = special_characters(@current_token)
       else
         token_type = -99
-        token_text = @current_token
+        @token_text = @current_token
         continue
       end
     end
-    token = MyToken.new(token_type, token_text)
+    token = MyToken.new(token_type, @token_text)
     return token
   end
 
+  # Identifies tokens containing strings or the while keyword
+  def string_kwrd
+    @token_text = @current_token
+    continue
+    while @current_token =~ /[A-Za-z]/ do
+      @token_text = @token_text + @current_token
+      continue
+    end
+    if @token_text == 'while'
+      return 0
+    else
+      return 1
+    end
+  end
+
+  # Identifies tokens containing integer or float values
+  def numbers
+    @token_text = @current_token
+    continue
+    while @current_token =~ /[0-9]/ do
+      @token_text = @token_text + @current_token
+      continue
+    end
+    if @current_token == '.'
+      @token_text = @token_text + @current_token
+      continue
+      while @current_token =~ /[0-9]/ do
+        @token_text = @token_text + @current_token
+        continue
+      end
+      return 3
+    else
+      return 2
+    end
+  end
+
+  # Identifiers tokens for comparisons
+  def comparisons(symbol)
+    case symbol
+    when '='
+      continue
+      if @current_token == '='
+        @token_text = "=="
+        continue
+        return 4
+      else
+        @token_text = '='
+        return -99
+      end
+    when '<'
+      continue
+      if @current_token == '='
+        @token_text = '<='
+        continue
+        return 6
+      else
+        @token_text = '<'
+        return 5
+      end
+    when '>'
+      continue
+      if @current_token == '='
+        @token_text = '>='
+        continue
+        return 8
+      else
+        @token_text = '>'
+        return 7
+      end
+    end
+  end
+
+  # Identifies valid special characters
+  def special_characters(symbol)
+    case symbol
+    when '('
+      @token_text = @current_token
+      continue
+      return 14
+    when ')'
+      @token_text = @current_token
+      continue
+      return 15
+    when '{'
+      @token_text = @current_token
+      continue
+      return 16
+    when '}'
+      @token_text = @current_token
+      continue
+      return 17
+    when ';'
+      @token_text = @current_token
+      continue
+      return 18
+    end
+  end
+
+  # A method that simplifies iteration and ignores whitespace in the input
   def continue
     @position += 1
     @current_token = @input[@position]
